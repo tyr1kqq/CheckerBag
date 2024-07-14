55c5a07197f9c650e39b74b1f8deb509617f04fa
Igor Turkin <igorturkinwhite@yandex.ru>
2024-07-14 12:20:43 +0200

    expension

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,4 +1,37 @@
 ﻿
+const images = document.querySelectorAll('.slider.img');
+let curentIndex = 0;
+
+function showImage(index) {
+    images[curentIndex].classList.remove('active');
+    images[index].classList.add('active');
+    curentIndex = index;
+}
+
+document
+    .querySelector('.slider.controlls')
+    .addEventListener('click', function (event) {
+        if (event.target.classList.contains('prev')) {
+            let index = currentIndex - 1;
+            if (index < 0) {
+                index = images.length - 1;
+            }
+            showImage(index);
+        } else if (event.target.classList.contains('next')) {
+            let index = currentIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            showImage(index);
+        }
+    });
+
+showImage(currentIndex);
+
+
+
+
+
 function Comment() {
     // Запросим имя
     this.author = prompt("Как вас зовут ?")
